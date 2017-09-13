e6b9e89f4ad0948d093e0ef2caf251cb3d00db48
aedobbyn <adobbs11@gmail.com>
2017-09-13 06:11:40 -0500

    add uncap_it

---
+++ R/uncap_it.R
@@ -0,0 +1,23 @@
+#' Uncap It
+#'
+#' Uncapitalize a vector or dataframe using one of the apply or purrr functions.
+#' @param e A vector or dataframe.
+#' @keywords uncapitalize
+#' @import stringr
+#' @export
+#' @examples
+#'
+#' string_to_tidy <- "A String that needs Tidying"
+#' uncap_it(string_to_tidy)
+
+
+uncap_it <- function(e, ...) {
+  e <- e %>%
+    stringr::str_to_lower() %>%
+    str_replace_all(., " ", "_")
+
+  return(e)
+}
+
+
+
