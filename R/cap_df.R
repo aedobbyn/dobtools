91c969e5854888574ee3500cc55d48245d5b0313
aedobbyn <adobbs11@gmail.com>
2017-09-19 10:57:19 -0500

    add cap_df

---
+++ R/cap_df.R
@@ -0,0 +1,15 @@
+#' Cap It
+#'
+#' Capitalize the column names in a dataframe.
+#' @param df A dataframe.
+#' @keywords capitalize
+#' @import Hmisc
+#' @export
+#' @examples
+#' mtcars_2 <- mtcars %>% cap_df()
+
+
+cap_df <- function(df) {
+  names(df) <- names(df) %>% map(cap_it) %>% as_vector()
+  return(df)
+}
