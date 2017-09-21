f0f8e2fd349e3981183e2e49b11b0f42445b16c0
aedobbyn <adobbs11@gmail.com>
2017-09-21 16:18:57 -0500

    start on not adding commas to year columns in style_numeric

--- R/style_numeric.R
+++ R/style_numeric.R
@@ -1,6 +1,7 @@
 #' Style Numeric
 #'
-#' Take a dataframe, round the digits to the thousandths, and optionally add commas.
+#' Take a dataframe, round the digits to the thousandths, and optionally add commas (except
+#' to year columns).
 #' @param df A dataframe.
 #' @param digits The number of digits to round decimals to.
 #' @param add_commas If TRUE, add commas in between 1000s.
@@ -14,12 +15,20 @@
 
 
 style_numeric <- function(df, digits = 3, add_commas = FALSE) {
+  year_cols <- names(df)[which(grepl("Year|year", names(df))==TRUE)]
+
+  # if (length(year_cols) > 0) {
+  #   non_year_cols <- names(df)[-which(names(df)==year_cols)]
+  # } else {
+  #   non_year_cols <- names(df)
+  # }
+
   df <- df %>% map_if(is.numeric, round, digits = digits) %>%
     as_tibble()
 
   if (add_commas == TRUE) {
     for (col in names(df)) {
-      if(is.numeric(df[[col]])) {
+      if(is.numeric(df[[col]] & !(col %in% year_cols))) {
         df[[col]] <- df[[col]] %>% scales::comma_format()()
       }
     }
