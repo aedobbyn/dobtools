2592a8c45e63dd358d27375c2623ff34ad1428a4
aedobbyn <adobbs11@gmail.com>
2017-09-29 18:01:30 -0500

    add replace column variables function

--- R/replace_names.R
+++ R/replace_names.R
@@ -1,19 +1,22 @@
-original_names <- c("Actions Cumsum", "Profit Cumsum", "Dealer Id", "Action Year", "Action Month")
-replacement_names <- c("Sum of Actions: All Time", "Sum of Profit: All Time", "Dealer ID",
-                       "Previous Action Year", "Previous Action Month")
+#' Replace Column Names
+#'
+#' Replace a vector of column names with another vector
+#' @param vec A vector of names to replace
+#' @param ref_df A reference dataframe that ties the vector of names to replace to its replacements
+#' @param orig_names The column name of the ref_df vector serving as the original names
+#' @param new The column name of the ref_df vector serving as the new names
+#' @keywords names
+#' @import tidyverse
+#' @export
+#' @examples
+#'
+#' original_names <- names(iris)
+#' replacement_names <- c("fee", "fi", "fo", "fum", "foo")
+#' name_df <- cbind(original_names, replacement_names) %>% as_tibble()
+#'
+#' names(iris) %>% replace_colnames(name_df, "original_names", "replacement_names")
 
-
-name_df <- cbind(original_names, replacement_names) %>% as_tibble()
-
-original_names_future <- c("Profit", "Last Action Profit", "Action Type", "Last Action Type",
-                           "Action Typeservice", "Gold Box Boolean1", "Is Heavy Metal CustYes")
-replacement_names_future <- c("Previous Action Profit", "Pre-Previous Action Profit", "Previous Action Type", "Pre-Previous Action Type",
-                              "Previous Action Type -- Service", "Gold Box Boolean -- Yes",
-                              "Is Heavy Metal Cust -- Yes")
-
-name_df_future <- cbind(original_names_future, replacement_names_future) %>% as_tibble()
-
-replace_names_base <- function(vec, ref_df, orig, new) {
+replace_colnames <- function(vec, ref_df, orig, new) {
   for (i in seq_along(vec)) {
     if (vec[i] %in% ref_df[[orig]]) {
       j <- which(ref_df[[orig]]==vec[i])
@@ -25,11 +28,4 @@ replace_names_base <- function(vec, ref_df, orig, new) {
   vec
 }
 
-replace_names <- function(vec, future_prediction = FALSE) {
-  vec <- replace_names_base(vec, name_df, "original_names", "replacement_names")
-  
-  if(future_prediction == TRUE) {
-    vec <- replace_names_base(vec, name_df_future, "original_names_future", "replacement_names_future")
-  }
-  vec
-}
+
