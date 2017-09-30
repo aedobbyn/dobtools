0a3d10131dccce19dd80fc1a62360e0e964b7006
aedobbyn <adobbs11@gmail.com>
2017-09-29 20:55:35 -0500

    remove filtering from run_rf

--- R/run_rf.R
+++ R/run_rf.R
@@ -3,8 +3,6 @@
 #' Given a random forest model, extract the importance coefficient and percent.
 #' @param predictor_vars A vector of predictors
 #' @param outcome_var A single outcome variable. Factor or numeric.
-#' @param filter_var A variable to filter
-#' @param filter_to A value in that filter variable to filter to.
 #' @param prob Probability or response outcome? If true, outcome variable must be a factor.
 #' @keywords rf
 #' @import ranger
@@ -14,18 +12,15 @@
 #'
 #' preds <- c("disp", "hp", "qsec")
 #' response <- "carb"
-#' mt_rf <- prep_rf(mtcars, preds, response, prob = FALSE)
+#' mt_rf <- run_rf(mtcars, preds, response, prob = FALSE, filter_var = "vs", filter_to = 0)
 
 
-prep_rf <- function(data, predictor_vars, outcome_var, n_trees = 500, filter_var = NULL,
-                    filter_to = NULL, prob = FALSE) {
+run_rf <- function(data, predictor_vars, outcome_var, n_trees = 500, prob = FALSE) {
 
   data <- data %>%
     mutate(
       row_num = row_number()
     )
-
-  out <- NULL
 
   both <- c(predictor_vars, outcome_var, "row_num")
   quo_both <- enquo(both)
@@ -35,17 +30,11 @@ prep_rf <- function(data, predictor_vars, outcome_var, n_trees = 500, filter_var
     select_(.dots = both) %>%
     na.omit()
 
-  if(!is.null(filter_to)) {
-    dat_forest <- dat_forest %>%
-      filter(!!filter_var == !!filter_to)
-  }
-
   # Keep 80% for training
   forest_train <- dat_forest %>%
     sample_frac(., 0.8)
 
   # The rest is for testing
-  # Save testing df as global variable
   forest_test <- dat_forest %>%
     filter(! (row_num %in% forest_train$row_num)) %>%
     select(-row_num)
@@ -54,7 +43,6 @@ prep_rf <- function(data, predictor_vars, outcome_var, n_trees = 500, filter_var
   forest_train <- forest_train %>%
     select(-row_num)
 
-  # browser()
   # Separate rf_preds with +s and take out row_num
   rf_preds_sep <- paste0(predictor_vars, collapse = " + ")
 
