40c7681339f1713a32dbf8149fa845dc879fc939
aedobbyn <adobbs11@gmail.com>
2017-10-04 16:35:22 -0500

    add group_equal

---
+++ R/group_equal.R
@@ -0,0 +1,38 @@
+#' Group Equal
+#'
+#' Split a dataframe column into equal-sized groups.
+#' @param df A dataframe
+#' @param col A numeric column. If the value here is outside the vector range, the `group` colum
+#' will be set to NA for that row.
+#' @param n_groups Number of groups to end.
+#' @keywords group
+#' @import tidyverse
+#' lubridate
+#' @export
+#' @examples
+#'
+#' groups <- group_equal(mtcars, "mpg", n_groups = 5)
+#' group_numeric(mtcars, "mpg", groups, add_first_last = TRUE)
+
+
+group_equal <- function(df, col, n_groups = 5) {
+  df <- df %>% drop_na(!!col) %>%
+    ungroup() %>%
+    arrange_(.dots = col) %>%
+    mutate(row_num = row_number())
+
+  n_total <- nrow(df)
+  rnge <- range(df[[col]])
+  n_per_group <- floor(n_total/n_groups)
+
+  vec <- NULL
+
+  for (g in 1:n_groups) {
+    this_upper_bound <- df[which(df[["row_num"]]
+                                 == (n_per_group * g)), ][[col]]
+    vec <- c(vec, this_upper_bound)
+  }
+
+  return(vec)
+}
+
