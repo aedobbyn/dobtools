62563430e3e74e160af36a6fefab797c796392ac
aedobbyn <adobbs11@gmail.com>
2017-10-11 17:32:49 -0500

    add nerdR

---
+++ R/nerdR.R
@@ -0,0 +1,12 @@
+#' But you already knew that
+#'
+#' Say it loud and clear. At the request of one [Kyle Gilbert](https://github.com/klye-g).
+#' @keywords nerd
+#' @export
+#' @examples
+#' nerdR()
+
+
+nerdR <- function() {
+  print("You're a nerd!")
+}
