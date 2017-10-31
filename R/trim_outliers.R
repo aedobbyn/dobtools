c2dde91484143247b96e92a42b9e61e92d4567fe
aedobbyn <adobbs11@gmail.com>
2017-10-31 16:47:25 -0500

    in trim_outliers, allow the user to specify whther they want to include or exlcude columns for trimming

--- R/trim_outliers.R
+++ R/trim_outliers.R
@@ -1,8 +1,10 @@
 #' Trim Outliers
 #'
-#' Remove outliers
+#' Remove outliers by either choosing a vector of column names to include for consideration or choosing a vector
+#' for exclusion. Either include or exclude must be non-NULL (not neither and not both).
 #' @param df A dataframe.
 #' @param cutoff A numeric standard deviation cutoff value.
+#' @param include Vector of variables to subject to cutting off.
 #' @param exclude Vector of variables not to subject to cutting off.
 #' @param keep_scaled Should the newly scaled numeric variables be kept in the output?
 #' @keywords trim
@@ -11,11 +13,31 @@
 #' @examples
 #'
 #' trim_outliers(iris, cutoff = 1, exclude = c("Species", "Sepal.Length"), keep_scaled = FALSE)
+#' trim_outliers(iris, cutoff = 2, include = c("Petal.Width", "Sepal.Length"), keep_scaled = TRUE)
+#'
+#' # Error -- exclude and include can't both be non-NULL
+#' trim_outliers(iris, cutoff = 2, exclude = "Species", include = c("Petal.Width", "Sepal.Length"), keep_scaled = TRUE)
+#'
+#' # Error -- exclude and include can't both be NULL
+#' trim_outliers(iris, cutoff = 2, keep_scaled = TRUE)
 
 
-trim_outliers <- function(df, cutoff = 1.96, exclude = NULL, keep_scaled = TRUE){
 
-  to_scale <- names(df)[!names(df) %in% exclude]
+trim_outliers <- function(df, cutoff = 1.96, exclude = NULL, include = NULL, keep_scaled = TRUE){
+
+  assertthat::assert_that(!(is.null(exclude) & is.null(include)),
+                          msg = "Either include or exclude must not be NULL.")
+
+  assertthat::assert_that(!(!is.null(exclude) & !(is.null(include))),
+                          msg = "Only one of include and exclude can be non-NULL.")
+
+  if (!is.null(exclude)) {
+    to_scale <- names(df)[!names(df) %in% exclude]
+  }
+
+  if (!is.null(include)) {
+    to_scale <- names(df)[names(df) %in% include]
+  }
 
   assertthat::assert_that(!(FALSE %in%
                               sapply(df[, which(names(df) %in% to_scale)], is.numeric)), msg = "All variables to scale must be numeric.")
