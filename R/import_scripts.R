91bccac1eefbc8d509e4925f8d87108ddb2be6e3
aedobbyn <adobbs11@gmail.com>
2018-01-10 17:11:09 -0600

    fail loudly if file path is wrong

--- R/import_scripts.R
+++ R/import_scripts.R
@@ -10,14 +10,15 @@
 #'
 #' @examples
 #'
-#' import_scripts(path = "./data")
-#' droids   # droids defined in data/get_droids.R in this package
-#' import_scripts(path = "foo")   # Should fail with a warning, not an error
+# import_scripts(path = "./data")
+#' droids   # droids defined in data/get_droids.R
+# import_scripts(path = "foo")   # Should fail with a warning, not an error
 
 import_scripts <- function(path, pattern = "*.R") {
   files <- list.files(path, pattern, ignore.case = TRUE)
   file_paths <- str_c(path, "/", files)
-  try_source <- possibly(source, otherwise = message("Can't find this file or path."))
+  try_source <- possibly(source, otherwise = message(paste0("Can't find this file or path: ")),
+                         quiet = FALSE)
 
   for (file in file_paths) {
     try_source(file)
