97b3e459e7c4f84c7f4630f2e1f7e744be42faea
aedobbyn <adobbs11@gmail.com>
2018-01-11 22:51:16 -0600

    test_it

---
+++ R/test_it.R
@@ -0,0 +1,26 @@
+
+# Return whether an individual test passed or not
+#'
+#' @param f The file
+#' @param verbose Should messages and warnings be suppressed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+
+
+test_it <- function(f, verbose = FALSE) {
+  if (verbose == FALSE) {
+    result <- suppressMessages(suppressWarnings(try(source(f), silent = TRUE)))
+  } else {
+    result <- try(source(f), silent = TRUE)
+  }
+
+  if (inherits(result, "try-error")) {
+    message(paste0(" --- ", f, " FAILED --- "))
+  } else {
+    message(paste0(" --- ", f, " PASSED --- "))
+  }
+}
