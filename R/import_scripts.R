5e8a178a8e969bbf472df0b19234ccca4af26726
aedobbyn <adobbs11@gmail.com>
2018-01-12 19:31:52 -0600

    clean up examples in import_scripts()

--- R/import_scripts.R
+++ R/import_scripts.R
@@ -9,16 +9,18 @@
 #'
 #' @examples
 #'
+#' # Import data
 #' import_scripts(path = "./data", pattern = ".rda")
-#' import_scripts(path = "./R", pattern = "get_droids.R")
-#' droids   # droids defined in data/get_droids.R
+#'
+#' # Import scripts
+#' import_scripts(path = "./data-raw", pattern = "get_droids.R")
 #' import_scripts(path = "foo")   # Should fail with a warning, not an error
 
 import_scripts <- function(path, pattern = "*.R") {
   files <- list.files(path, pattern, ignore.case = TRUE)
   file_paths <- str_c(path, "/", files)
 
-  try_source <- possibly(source, otherwise = "bar",
+  try_source <- possibly(source, otherwise = "File not found :(",
     quiet = TRUE)
 
   for (file in file_paths) {
