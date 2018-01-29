ab0f15459c6c41e3224f1a0615b3eece055efeab
aedobbyn <adobbs11@gmail.com>
2018-01-29 14:25:59 -0600

    change import_scripts to messsage when a file's not found rather than print

--- R/import_scripts.R
+++ R/import_scripts.R
@@ -21,7 +21,7 @@ import_scripts <- function(path, pattern = "*.R", quiet = TRUE) {
   files <- list.files(path, pattern, ignore.case = TRUE)
   file_paths <- str_c(path, "/", files)
 
-  try_source <- possibly(source, otherwise = print("File not found :("),
+  try_source <- possibly(source, otherwise = message("File not found :("),
     quiet = TRUE)
 
   for (file in file_paths) {
