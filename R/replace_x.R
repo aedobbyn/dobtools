db4430e38c247e0d6c3067eddd7e45a39503db0c
aedobbyn <adobbs11@gmail.com>
2018-03-12 16:30:21 -0500

    add replace_x

---
+++ R/replace_x.R
@@ -0,0 +1,23 @@
+#' Replace x
+#'
+#' @description Replace missing or NA values with a replacement
+#' @param x Element to replace
+#' @param replacement Defaults to NA_character_
+#'
+#' @export
+#'
+#' @examples
+#'
+#' vec <- c("a", NA, "b")
+#' vec %>% map(replace_x, replacement = "foo")
+
+
+replace_x <- function(x, replacement = NA_character_) {
+  if(is.null(x) || is.na(x)) {
+    replacement
+  } else {
+    x
+  }
+}
+
+
