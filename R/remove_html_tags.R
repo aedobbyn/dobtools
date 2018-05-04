5228e79188ab10f53f66eba05fffa927cc2411de
aedobbyn <adobbs11@gmail.com>
2018-05-04 13:56:44 -0400

    add removal of newlines and amp tags to remove html tags

--- R/remove_html_tags.R
+++ R/remove_html_tags.R
@@ -1,6 +1,6 @@
 #' Remove HTML tags
 #'
-#' @description Take a string and remove HTML tags from hit. Does not remove \code{&amp}s or newlines
+#' @description Take a string and remove HTML tags from hit.
 #'
 #' @param h_str A string possibly containing HTML tags
 #' @return Cleaned string
@@ -8,10 +8,16 @@
 #'
 #' @examples
 #'
-#' to_clean <- "<li> some <b>bullet</b> </li>"
+#' to_clean <- "<li> some \n <b>bullet</b> </li>"
 #' remove_html_tags(to_clean)
 
 remove_html_tags <- function(h_str) {
-  return(gsub("<.*?>", "", h_str))
+  out <- stringr::str_replace_all(h_str, "<.*?>", "") %>%
+    stringr::str_replace_all("\\n", "") %>%
+    stringr::str_replace_all("\\r", "") %>%
+    stringr::str_replace_all("\\t", "") %>%
+    stringr::str_replace_all("&amp", "&") %>%
+    stringr::str_replace_all("&nbsp", " ")
+  return(out)
 }
 
